94d1bde0b275cda1d61f42b65e079ec51259b9ae
fourmond <antfourmond@gmail.com>
2026-03-27 15:37:27 +0000

    une deuxieme couche nous donne 98.9% d'accuracy git add protoN.py

--- src/protoN.py
+++ src/protoN.py
@@ -4,7 +4,7 @@ class Neuron:
     # Pour choisir la taille des différentes couches on va tester différentes config pour l'instant
     # Trop de neuronnes entraineraot un sur apprentissage 
     # Mais pas assez entraînerait un sous apprentissage 
-    def __init__(self, input_size, learnig_rate=0.001, hidden_size=32 , l2_lambda=0.001):
+    def __init__(self, input_size, learnig_rate=0.001, hidden1_size=32 ,hidden2_size=16, l2_lambda=0.001):
         self.lr = learnig_rate
         # On a décidé de rajouter une couche pour que notre modèle distingues des patterns complexes pas que linéaires
         # On ne multiplie plus par 0.1 mais par un nombre qui s'adapte à la taille de la couches
@@ -12,11 +12,14 @@ class Neuron:
         self.l2_lambda = l2_lambda
 
         # Taille de la couche en fonction des inputs, initialisation des poids et du biais 
-        self.W1 = np.random.randn(input_size, hidden_size) * np.sqrt(2.0 / input_size)
-        self.b1 = np.zeros((1, hidden_size))
-        # Couche cachée 1 
-        self.W2 = np.random.randn(hidden_size, 1) * np.sqrt(2.0 / hidden_size)
-        self.b2 = np.zeros((1, 1))
+        self.W1 = np.random.randn(input_size, hidden1_size) * np.sqrt(2.0 / input_size)
+        self.b1 = np.zeros((1, hidden1_size))
+        # Couche cachée 1
+        self.W2 = np.random.randn(hidden1_size, hidden2_size) * np.sqrt(2.0 / hidden1_size)
+        self.b2 = np.zeros((1, hidden2_size))
+        # Couche cachée 2
+        self.W3 = np.random.randn(hidden2_size, 1) * np.sqrt(2.0 / hidden2_size)
+        self.b3 = np.zeros((1, 1))
 
         self.mean = None
         self.std = None
@@ -33,7 +36,7 @@ class Neuron:
     def _init_adam(self):
         self.m = {}
         self.v = {}
-        for name in ['W1', 'b1', 'W2', 'b2']:
+        for name in ['W1', 'b1', 'W2', 'b2', 'W3', 'b3']:
             param = getattr(self, name)
             # Pour les poids : 
             # Accumule la direction des gradients (fisrt moment)
@@ -71,8 +74,11 @@ class Neuron:
         # La sortie de la première couche devient l'entrée de la deuxième
 
         self.z2 = self.a1.dot(self.W2) + self.b2
+        self.a2 = self.relu(self.z2)
 
-        return self.sigmoid(self.z2)
+        self.z3 = self.a2.dot(self.W3) + self.b3
+
+        return self.sigmoid(self.z3)
     
     # mesure quant bien la prediction des donnes sont arrivées en rapport à le resultat réel
     # plus proches de 0 meilleur la prediction
@@ -91,23 +97,28 @@ class Neuron:
         # calcul erreur prédiction - vérité
         # dérivation de loss + signmoid
         # Le gradient est la dérivée de la loss
-        dZ2 = model - y
+        dZ3 = model - y
 
+        dW3 = 1/s * np.dot(self.a2.T, dZ3) + self.l2_lambda * self.W3
+        db3 = 1/s * np.sum(dZ3, axis=0, keepdims=True)
+
+        dA2 = dZ3.dot(self.W3.T)
+        # Correction des neuronnes qui ont servi seulement, les autres morts = pas de correction
+        dZ2 = dA2 * self.relu_derivative(self.z2)
         dW2 = 1/s * np.dot(self.a1.T, dZ2) + self.l2_lambda * self.W2
         db2 = 1/s * np.sum(dZ2, axis=0, keepdims=True)
-
-        dA1 = dZ2.dot(self.W2.T)
-
-        # Correction des neuronnes qui ont servi seulement, les autres morts = pas de correction
-        dZ1 = dA1 * self.relu_derivative(self.z1)
         
+        dA1 = dZ2.dot(self.W2.T)
+        dZ1 = dA1 * self.relu_derivative(self.z1)
         dW1 = 1/s * np.dot(data.T, dZ1) + self.l2_lambda * self.W1
         db1 = 1/s * np.sum(dZ1, axis=0, keepdims=True)
-        return {'W1': dW1, 'b1': db1, 'W2': dW2, 'b2': db2}
+        
+        return {'W1': dW1, 'b1': db1, 'W2': dW2, 'b2': db2, 'W3': dW3, 'b3': db3}
+
     
     def update(self, grads):
         self.t += 1
-        for name in ['W1', 'b1', 'W2', 'b2']:
+        for name in ['W1', 'b1', 'W2', 'b2', 'W3', 'b3']:
             # Limite sur les gradients pour eviter l'explosion des gradients
             g = np.clip(grads[name], -5.0, 5.0)
             # Récupération de la direction moyenne en intégrant le gradient courant
